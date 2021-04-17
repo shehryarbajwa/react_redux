311b51d3ecba767cc790e30feba20145ce01b0ff
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2021-04-17 23:36:45 +0500

    convert component added

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
+import Translate from "./components/Translate";
 
 const items = [
   {
@@ -35,19 +36,9 @@ const options = [
 //Pass down a function callback as props to a component
 
 const App = () => {
-  const [selected, setSelected] = useState(options[0]);
-  const [showDropdown, setShowDropdown] = useState(true)
-
   return (
     <div>
-      <button onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
-      { showDropdown ? 
-       <Dropdown
-       selected={selected}
-       options={options}
-       onSelectedChange={setSelected}
-     /> : null
-      }
+      <Translate />
     </div>
   );
 };
